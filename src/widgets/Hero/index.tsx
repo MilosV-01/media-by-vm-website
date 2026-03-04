c8ee696b2c44dde2df7a2f466b50e0683698558b
Milos Vukmirovic <vukmirovicm48@gmail.com>
2026-03-04 03:37:00 +0100

    Improve hero mobile text + smaller CTA buttons

--- src/widgets/Hero/index.tsx
+++ src/widgets/Hero/index.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
           className="relative z-20 mt-[-5vw] w-full text-center text-[4.5vw] md:text-[6vw] md:leading-[1.2] font-extrabold text-bold-1 max-w-[95%]"
           style={{ opacity }}
         >
-          Vukma Marketing
+          VUKMA Marketing
         </motion.h1>
         <motion.h2 ref={heading2} className="z-20 text-[1.7vw] md:text-[3vw] font-medium text-text-1/7 5" style={{ opacity }} >
           Marketing koji gradi brend, privlači klijente i donosi rezultate.
